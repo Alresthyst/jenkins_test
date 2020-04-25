a39d43533f9674c6a84b43f613df2e70def423de
kwk1602 <kwk1602@gmail.com>
2020-04-26 02:07:12 +0900

    jenkins test 6

--- parse_argu.py
+++ parse_argu.py
@@ -15,7 +15,7 @@ if options.server_mode == "release":
 elif options.server_mode == "develop":
     server_yml_name = "develop_config.yml"
 
-print("Server mode: " + options.server_mode + " :)\n")
-command_str = "python server.py -f " + server_yml_name
+print("Server mode: " + options.server_mode + " ^^\n")
+command_str = "python3 server.py -f " + server_yml_name
 os.system(command_str)
 
