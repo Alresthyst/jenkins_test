a617fbd0e666abf7ac0c7e5c30ccc5ef2b297323
kwk1602 <kwk1602@gmail.com>
2020-04-26 02:46:43 +0900

    jenkins test 13

--- properties/jenkins_bulid.sh
+++ properties/jenkins_bulid.sh
@@ -1,17 +1,21 @@
 export mode=develop
 
 
+if [ $(docker ps -q -f ancestor=$mode) > 0 ]; then
+    docker stop $(docker ps -q -f ancestor=$mode)
+else
+  echo "There is running old version of server"
+fi
+
+
 docker build -t $mode .
+
+
 if [ $(docker images -q -f dangling=true) > 0 ]; then
     docker rmi -f $(docker images -q -f dangling=true)
 else
   echo "There is no dangling images"
 fi
 
-if [ $(docker ps -q -f ancestor=$mode) > 0 ]; then
-    docker stop $(docker ps -q -f ancestor=$mode)
-else
-  echo "There is running old version of server"
-fi
 
 docker run -p 8180:8180 -e server_mode=develop -d develop
