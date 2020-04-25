a08dfdd91815ed6a4c5d80a9cbc4782e9d954152
kwk1602 <kwk1602@gmail.com>
2020-04-26 02:37:35 +0900

    jenkins test 11

--- properties/jenkins_bulid.sh
+++ properties/jenkins_bulid.sh
@@ -1,3 +1,9 @@
+#! /bin/bash
+
 docker build -t develop .
-docker rmi -f $(docker images -q -f dangling=true)
+if [ $(docker images -q -f dangling=true) > 0 ]; then
+    docker rmi -f $(docker images -q -f dangling=true)
+else
+  echo "There is no dangling images"
+fi
 docker run -p 8180:8180 -e server_mode=develop develop
